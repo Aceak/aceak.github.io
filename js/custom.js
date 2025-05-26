a92d1191296f9032ed62740b8db6e2d577d30a54
Aceak <kaslim@foxmail.com>
2025-05-26 17:35:57 +0800

    Site updated: 2025-05-26 17:35:57

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-/* global hexo */
-const js = hexo.extend.helper.get('js').bind(hexo);
-
-hexo.extend.injector.register('head_end', () => {
-    return js('https://analyz.stxwer.top/api/script.js');
-});
